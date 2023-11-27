6fdc3ab57bbcf62672a9ce1ec13c9a0b37951f30
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2023-11-27 14:26:26 -0800

    Fixed rollDie code errors

--- ts/dice.ts
+++ ts/dice.ts
@@ -30,13 +30,18 @@ window.onload = function(){
 
 function createNewGame(){
     //set player 1 and player 2 scores to 0
-    
+    let player1Score = (<HTMLInputElement>document.getElementById("score1")).value;
+    let player2Score = (<HTMLInputElement>document.getElementById("score2")).value;
+    player1Score = "0";
+    player2Score= "0";
+
     //verify each player has a name
     //if both players don't have a name display error
     let player1Name = (<HTMLInputElement>document.getElementById("player1")).value;
     let player2Name = (<HTMLInputElement>document.getElementById("player2")).value;
     if (player1Name == "" || player2Name == "") {
         alert("Please enter a name!");
+        return false;
     }
 
     //if both players do have a name start the game!
@@ -52,24 +57,53 @@ function rollDie():void{
     let currTotal = parseInt((<HTMLInputElement>document.getElementById("total")).value);
     
     //roll the die and get a random value 1 - 6 (use generateRandomValue function)
+    let dieRoll = generateRandomValue(1, 6);
 
     //if the roll is 1
+    if (dieRoll == 1) {
     //  change players
+    changePlayers();
+    }
     //  set current total to 0
+    currTotal = 0;
     
     //if the roll is greater than 1
+    if (dieRoll > 1) {
     //  add roll value to current total
-
+        currTotal += dieRoll;
+    }
     //set the die roll to value player rolled
+    let roll = document.getElementById("roll");
+    roll.innerText = dieRoll.toString();
+
     //display current total on form
+    let total: HTMLInputElement = document.getElementById("total") as HTMLInputElement;
+    total.value = currTotal.toString();
 }
 
 function holdDie():void{
     //get the current turn total
+    let currentTotal = parseInt(document.getElementById("total").value);
     //determine who the current player is
-    //add the current turn total to the player's total score
+    let currentPlayerName = (<HTMLElement>document.getElementById("current")).innerText;
+    let player1Name = (<HTMLInputElement>document.getElementById("player1")).value;
+    let player2Name = (<HTMLInputElement>document.getElementById("player2")).value;
 
+    //add the current turn total to the player's total score
+    if (currentPlayerName === player1Name) {
+        let score1 = parseInt(document.getElementById("score1").value);
+        score1 += currentTotal;
+        document.getElementById("score1").value = score1.toString();
     //reset the turn total to 0
+        currentTotal = 0;
+      } 
+      else if (currentPlayerName === player2Name){
+        let score2 = parseInt(document.getElementById("score2").value);
+        score2 += currentTotal;
+        document.getElementById("score2").value = score2.toString();
+    //reset the turn total to 0
+      currentTotal = 0;
+      }
 
     //change players
     changePlayers();
